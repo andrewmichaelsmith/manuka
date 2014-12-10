9aa670459cb1a85a69c907f77d027f251de55c65
Andrew Smith <me@andrewmichaelsmith.com>
2014-12-10 23:07:21 +0000

    off bye-bye-script, tried and failed to get ports forwarded. --net hosts and links don't co-operate

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
 #/usr/bin/env bash
-docker run -d --name dionaea andrewmichaelsmith/docker-dionaea
-docker run -d --name kippo andrewmichaelsmith/docker-kippo
+
+# Kippo
+
+docker rm -f kippo
+#docker pull andrewmichaelsmith/docker-kippo
+#docker run -d --publish-all --name kippo andrewmichaelsmith/docker-kippo
+docker run -d --publish-all --name kippo docker_kippo
+
+# Dionaea
+##
+## Well hacky. We stick dionaea on --net host
+## so it gets all the ports on the host and
+## then in the image forward 22 -> Kippo Container
+##
+docker rm -f dionaea
+#docker pull andrewmichaelsmith/docker-dionaea
+#docker run -d --publish-all --name dionaea andrewmichaelsmith/docker-dionaea
+docker run --link kippo:kippo --net host -d --publish-all --name dionaea docker_dionaea
+
