3244dfd0cfbc4ee464cebed6c724f9759238abfa
Andrew Smith <me@andrewmichaelsmith.com>
2014-12-11 20:00:41 +0000

    Just explicitly expose ports. TODO: UDP

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 docker rm -f kippo
 #docker pull andrewmichaelsmith/docker-kippo
 #docker run -d --publish-all --name kippo andrewmichaelsmith/docker-kippo
-docker run -d --publish-all --name kippo docker_kippo
+docker run -d -p 22:2222 --name kippo docker_kippo
 
 # Dionaea
 ##
@@ -16,5 +16,5 @@ docker run -d --publish-all --name kippo docker_kippo
 docker rm -f dionaea
 #docker pull andrewmichaelsmith/docker-dionaea
 #docker run -d --publish-all --name dionaea andrewmichaelsmith/docker-dionaea
-docker run --link kippo:kippo --net host -d --publish-all --name dionaea docker_dionaea
+docker run -d -p 21:21 -p 42:42 -p 80:80 -p 135:135 -p 443:443 -p 445:445 -p 1433:1433 -p 3306:3306 -p 5060:5060 -p 5061:5061 --name dionaea docker_dionaea
 
