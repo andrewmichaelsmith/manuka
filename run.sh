87b22306b731b57eda860dad8f7ae4d3477e2b1b
Andrew Smith <me@andrewmichaelsmith.com>
2014-12-11 20:10:46 +0000

    use registry.hub.docker.com images, not local ones

--- run.sh
+++ run.sh
@@ -3,14 +3,12 @@
 # Kippo
 
 docker rm -f kippo
-#docker pull andrewmichaelsmith/docker-kippo
-#docker run -d --publish-all --name kippo andrewmichaelsmith/docker-kippo
-docker run -d -p 22:2222 --name kippo docker_kippo
+docker pull andrewmichaelsmith/docker-kippo
+docker run -d -p 22:2222 --name kippo andrewmichaelsmith/docker-kippo
 
 # Dionaea
 
 docker rm -f dionaea
-#docker pull andrewmichaelsmith/docker-dionaea
-#docker run -d --publish-all --name dionaea andrewmichaelsmith/docker-dionaea
-docker run -d -p 21:21 -p 42:42 -p 80:80 -p 135:135 -p 443:443 -p 445:445 -p 1433:1433 -p 3306:3306 -p 5060:5060 -p 5061:5061 --name dionaea docker_dionaea
+docker pull andrewmichaelsmith/docker-dionaea
+docker run -d -p 21:21 -p 42:42 -p 80:80 -p 135:135 -p 443:443 -p 445:445 -p 1433:1433 -p 3306:3306 -p 5060:5060 -p 5061:5061 --name dionaea andrewmichaelsmith/docker_dionaea
 
