392d3f9ed094985313e99f43c9302a1f8187180c
Andrew Smith <me@andrewmichaelsmith.com>
2014-12-17 09:19:54 +0000

    Use new names

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ sudo service ssh reload
 
 # Kippo
 docker rm -f kippo
-docker pull andrewmichaelsmith/docker-kippo
+docker pull andrewmichaelsmith/kippo
 docker run --restart=always -d -p 22:2222 --name kippo andrewmichaelsmith/docker-kippo
 
 # Dionaea
 docker rm -f dionaea
-docker pull andrewmichaelsmith/docker-dionaea
+docker pull andrewmichaelsmith/dionaea
 docker run --restart=always -d -p 21:21 -p 42:42 -p 80:80 -p 135:135 -p 443:443 -p 445:445 -p 1433:1433 -p 3306:3306 -p 5060:5060 -p 5061:5061 --name dionaea andrewmichaelsmith/docker_dionaea
 
