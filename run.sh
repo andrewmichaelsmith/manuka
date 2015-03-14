b66d6f153240c4ca179db3adc15d23a785f17cd4
Andrew Smith <me@andrewmichaelsmith.com>
2015-03-14 18:42:50 +0000

    Use docker-compose

--- run.sh
+++ run.sh
@@ -21,37 +21,38 @@ mkdir -p $DIR/var/dionaea/bistreams
 sudo chown $KIPPO_UID $DIR/var/kippo 
 sudo chown -R $DIONAEA_UID $DIR/var/dionaea
 
+sudo docker-compose up -d
 
-# Kippo
-docker rm -f kippo
-#docker pull andrewmichaelsmith/kippo
-docker run --restart=always \
-	-v `pwd`/var/kippo:/vol/kippo/ \
-	-w /vol/kippo \
-	-d \
-	-p 22:2222 \
-	--name kippo \
-	andrewmichaelsmith/kippo:latest
-
-# Dionaea
-docker rm -f dionaea
-#docker pull andrewmichaelsmith/dionaea
-docker run --restart=always \
-	-v `pwd`/var/dionaea:/vol/dionaea \
-	-d \
-	--name dionaea \
-	-p 21:21 \
-	-p 42:42 \
-	-p 80:80 \
-	-p 135:135 \
-	-p 443:443 \
-	-p 445:445 \
-	-p 1433:1433 \
-	-p 3306:3306 \
-	-p 5060:5060 \
-	-p 5061:5061 \
-	-p 69:69/udp \
-	-p 5060:5060/udp \
-	andrewmichaelsmith/dionaea:latest
+## Kippo
+#docker rm -f kippo
+##docker pull andrewmichaelsmith/kippo
+#docker run --restart=always \
+#	-v `pwd`/var/kippo:/vol/kippo/ \
+#	-w /vol/kippo \
+#	-d \
+#	-p 22:2222 \
+#	--name kippo \
+#	andrewmichaelsmith/kippo:latest
+#
+## Dionaea
+#docker rm -f dionaea
+##docker pull andrewmichaelsmith/dionaea
+#docker run --restart=always \
+#	-v `pwd`/var/dionaea:/vol/dionaea \
+#	-d \
+#	--name dionaea \
+#	-p 21:21 \
+#	-p 42:42 \
+#	-p 80:80 \
+#	-p 135:135 \
+#	-p 443:443 \
+#	-p 445:445 \
+#	-p 1433:1433 \
+#	-p 3306:3306 \
+#	-p 5060:5060 \
+#	-p 5061:5061 \
+#	-p 69:69/udp \
+#	-p 5060:5060/udp \
+#	andrewmichaelsmith/dionaea:latest
 
 echo "Done, files in `pwd`/var"
