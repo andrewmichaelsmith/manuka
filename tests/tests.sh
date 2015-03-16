f31fe49180715399b344f45dca7019d14848c294
Andrew Smith <me@andrewmichaelsmith.com>
2015-03-16 23:01:46 +0000

    make tests use root@ for login

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,3 +1,3 @@
 sudo nmap  -d -p 445 127.0.0.1 --script=smb-vuln-ms10-061
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R localhost && ssh localhost
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R localhost && ssh root@localhost
 
