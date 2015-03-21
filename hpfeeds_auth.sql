d34150a6a9bc494976f9e7eb6fe1785a46fa5728
Andrew Smith <me@andrewmichaelsmith.com>
2015-03-21 12:49:37 +0000

    hpfeeds for kippo now works

---
+++ hpfeeds_auth.sql
@@ -0,0 +1,2 @@
+insert into authkeys (owner, ident, secret, pubchans, subchans) values ('owner', 'dionaea', 'dionaea', '["dionaea.captures", "dionaea.shellcodeprofiles", "dionaea.dcerpcrequests", "mwbinary.dionaea.sensorunique"]', '[]');
+insert into authkeys (owner, ident, secret, pubchans, subchans) values ('owner', 'kippo', 'kippo', '["kippo.sessions"]', '[]');
